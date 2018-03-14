37cc343624b42195d55f0cdd37047f3965e87b78
nenu <nenu@nenu.fr>
2018-03-14 11:43:20 +0100

    add PWA spec

--- src/html.js
+++ src/html.js
@@ -31,7 +31,6 @@ export default class HTML extends React.Component {
           />
           {this.props.headComponents}
           <link rel="shortcut icon" href={favicon} />
-          <link rel="manifest" href="/manifest.json" />
           {css}
         </head>
         <body>
